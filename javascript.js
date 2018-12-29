b4ff675787a7ac1b54af7f5cd25cc0e79953bc74
Tiffany Ray <tiffanyraylee@gmail.com>
2018-12-29 16:00:36 -0600

    added a comment for the nav function

---
+++ javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+
+/* Nav bar function */
+  $('.burger').on('click', () => {
+    $('.nav-menu').fadeToggle('slow');
+  }).on('mouseleave', () => {
+    $('.nav-menu').hide();
+  });
+
+
+
+
+
+
+});
