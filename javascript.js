669dc63e5a8c966743f56246b73666f22387e2f6
Tiffany Ray <tiffanyraylee@gmail.com>
2018-12-30 16:31:16 -0600

    added javascript to make photo slider work

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+let $deviceWidth = $(window).width();
+
 
 /* Nav bar function */
   $('.burger').on('click', () => {
@@ -8,8 +10,39 @@ $(document).ready(() => {
   });
 
 
+  /* image slider */
+
+  function slideShow() {
+    /* configurations */
+    let animationSpeed = 1000;
+    let pause = 5000;
+    let currentSlide = 1;
+
+    /* cache DOM */
+    let $slider = $('#slider');
+    let $slideContainer = $slider.find('.slides');
+    let $slides = $slideContainer.find('.slide');
+    let $allSlides = $slideContainer.find('.slide').toArray();
+
+    let interval;
+
+    function startSlider() {
+      interval = setInterval(function() {
+        $slideContainer.animate({'margin-left' : '-='+$deviceWidth}, animationSpeed, function () {
+          currentSlide++;
+          if(currentSlide === $slides.length) {
+            currentSlide = 1;
+            $slideContainer.css('margin-left', 0);
+          }
+          });
+      }, pause);
+    };
+    startSlider()
 
 
 
+  /* End of Image slide function */
+  };
+  slideShow();
 
-});
+}); /* End of document ready function */
